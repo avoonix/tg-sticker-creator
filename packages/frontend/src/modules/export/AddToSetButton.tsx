76d80980f580ab5fbc736f0b60a5fdbcb38a8816
Avoo <git@avoonix.com>
2022-10-18 19:42:58 +0200

    redirect after save

--- packages/frontend/src/modules/export/AddToSetButton.tsx
+++ packages/frontend/src/modules/export/AddToSetButton.tsx
@@ -1,6 +1,7 @@
 import { Button, Text } from "@adobe/react-spectrum";
 import Export from "@spectrum-icons/workflow/Export";
 import { useAtom } from "jotai";
+import { useRouter } from "next/router";
 import { FC, useCallback } from "react";
 import { Lottie, optimizeFilesize } from "tg-sticker-creator";
 import { paletteAtom } from "../palette/ColorList";
@@ -18,22 +19,24 @@ const AddToSetButton: FC<Props> = (props) => {
   const [colors] = useAtom(paletteAtom);
   const [sticker] = useAtom(stickerAtom);
   const [auth] = useAtom(authAtom);
+  const router = useRouter();
 
   const save = useCallback(async () => {
     if (!auth.data || !auth.type) throw new Error("missing auth");
-    console.log({ config, colors, sticker, auth });
+    const file = await gzip(optimizeFilesize(props.lottie.clone()));
+    console.log("size in KiB", file.size / (1 << 10));
     const response = await saveSticker({
       settings: JSON.stringify(config),
       palette: JSON.stringify(colors),
-      file: await gzip(optimizeFilesize(props.lottie.clone())),
+      file,
       emojis: sticker.emojis.map((e) => e.emoji).join(""),
       authData: auth.data,
       authType: auth.type,
       sticker: sticker.id,
     });
 
+    router.push("/"); // TODO: close gui if used from within bot
     console.log("response", response);
-    // TODO
   }, [config]);
 
   return (
