2000038dc095763f39ee7762ff440d58fa292945
Avoo <git@avoonix.com>
2022-10-19 10:02:19 +0200

    fix lint error

--- packages/frontend/src/modules/export/AutoDownloader.tsx
+++ packages/frontend/src/modules/export/AutoDownloader.tsx
@@ -19,7 +19,7 @@ const AutoDownloader: FC = (props) => {
         fps,
         backgroundColor: "#000000",
       },
-      (p) => {},
+      () => null,
     ).then(() => {
       console.log("duration", (performance.now() - start) / 1000, "s");
     });
